a03a058977167701ad04d74e523a57f75acb484a
DhammaratnaIngale <123735909+DhammaratnaIngale@users.noreply.github.com>
2023-01-28 00:49:18 +0530

    BasicMethod

---
+++ sampleTask1/src/sampleTask1pack/BasicMethods.java
@@ -0,0 +1,14 @@
+package sampleTask1pack;
+
+public class BasicMethods {
+	
+	public static void main(String[]args) {
+	System.out.println("ABCD");
+	System.out.println("1234");
+	System.out.println("PQRS");
+	System.out.println("5678");
+	System.out.println("LMNO");
+	
+
+	}
+}
